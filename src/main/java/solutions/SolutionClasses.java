24c210e5ee5f6963af10371e9def23d15ff9f920
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-05-19 12:08:04 +0100

    longestSeq complete

--- src/main/java/solutions/SolutionClasses.java
+++ src/main/java/solutions/SolutionClasses.java
@@ -1,5 +1,59 @@
 package solutions;
 
-public class SolutionClasses {
 
+import java.util.*;
+
+
+public class SolutionClasses {
+    //Implement the function longestSequence(sequence)which takes a string of letters and finds
+    //the longest sequence where the same letter occurs continuously.
+    //The function must return an object literal {c: n} where c is the lowercase character and n is the
+    //length of this sub-sequence.
+    //If there are multiple characters with a continuous sequence of the same length, return the
+    //information for the letter which occurs first alphabetically.
+
+    NavigableMap<Character, Integer> lettersFound = new TreeMap<Character, Integer>();
+    public String longestSequence(String sequence){
+
+        int count;
+        String result = " ";
+        Character currentChar;
+        Character lastChar = ' ';
+        int accumulator = 0;
+        sequence = sequence.toLowerCase();
+
+        for (int i = 0; i < sequence.length(); i++){ // dghhhmhmx
+
+            currentChar = sequence.charAt(i);
+            if (!Character.isAlphabetic(currentChar)){
+                continue;
+            }
+            if(lettersFound.containsKey(currentChar)){
+                if (currentChar == lastChar){
+                    accumulator += 1;
+                    System.out.println(accumulator);
+                    if (accumulator >= lettersFound.get(currentChar)){
+                        lettersFound.put(currentChar, accumulator);
+                    }
+                }
+                else {
+                    accumulator = 1;
+                }
+            }
+            else {
+               accumulator = 1;
+               lettersFound.put(currentChar, 1);
+            }
+
+            lastChar = currentChar;
+        }
+        System.out.println(lettersFound);
+        int maxValue = Collections.max(lettersFound.values());
+        for (Map.Entry<Character,Integer> entry : lettersFound.descendingMap().entrySet()){
+            if (entry.getValue() == maxValue){
+                result = String.format("{%s : %s}", entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/solutions/SolutionClassTest.java
+++ src/test/java/solutions/SolutionClassTest.java
@@ -1,4 +1,34 @@
 package solutions;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SolutionClassTest {
+    SolutionClasses implement = new SolutionClasses();
+
+    @Test
+    public void testSequence(){
+        var actualResult = implement.longestSequence("hello");
+        Assertions.assertEquals("{l : 2}", actualResult);
+    }
+    @Test
+    public void testSequence2(){
+        var actualResult = implement.longestSequence("h e l l o ");
+        Assertions.assertEquals("{l : 2}", actualResult);
+    }
+    @Test
+    public void testSequence3(){
+        var actualResult = implement.longestSequence("dghhhmhmx");
+        Assertions.assertEquals("{h : 3}", actualResult);
+    }
+    @Test
+    public void testSequence4(){
+        var actualResult = implement.longestSequence("dhkkhhKKKt");
+        Assertions.assertEquals("{k : 3}", actualResult);
+    }
+    @Test
+    public void testSequence5(){
+        var actualResult = implement.longestSequence("aBbBadddadd");
+        Assertions.assertEquals("{b : 3}", actualResult);
+    }
 }
